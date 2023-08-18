a5ada8d01d4e882a442e4159c40fe21515297d2d
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-18 21:47:13 +0200

    Simplelightbox installed with npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const itemsArray = [];
+
+galleryItems.forEach(item => {
+  let listItem = document.createElement('li');
+  itemsArray.push(listItem);
+  listItem.insertAdjacentHTML(
+    'beforeend',
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+gallery.append(...itemsArray);
+gallery.addEventListener('click', clickImage);
+
+function clickImage(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery li a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
