3c122d65e0d102a92d42ac089e937e17ce40f4d0
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-22 17:44:42 +0200

    Vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const _ = require('lodash');
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+function onPlay() {
+  player
+    .getCurrentTime()
+    .then(() =>
+      localStorage.setItem(
+        'videoplayer-current-time',
+        `${player.getCurrentTime()}`
+      )
+    );
+
+  // console.log(localStorage.getItem('videoplayer-current-time'));
+}
+
+window.addEventListener('load', () =>
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+);
