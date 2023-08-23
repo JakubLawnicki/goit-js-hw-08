0596e48e2d9782dc7a7cf3fa74a4a456ffb2be4d
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-23 23:53:05 +0200

    Feedback file

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+const storageObj = { email: '', message: '' };
+let parsedObj;
+
+function createStorage() {
+  storageObj.email = emailInput.value;
+  storageObj.message = messageInput.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageObj));
+}
+
+form.addEventListener('input', throttle(createStorage, 500));
+
+window.addEventListener('load', () => {
+  parsedObj = JSON.parse(localStorage.getItem('feedback-form-state'));
+  emailInput.value = parsedObj.email;
+  messageInput.value = parsedObj.message;
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  (emailInput.value = ''), (messageInput.value = '');
+  console.log(parsedObj);
+});
